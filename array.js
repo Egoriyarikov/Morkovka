decb470bf3b18b3ad3598d6a823ac0009d9fb7a2
Egoriyarikov <Egoriyyarikov@gmail.com>
2025-04-20 11:54:25 +0300

    feat: array hahaha

---
+++ array.js
@@ -0,0 +1,17 @@
+var hahaha = ["a", "b", "c", "d", "e", "f"];
+
+// for (var i = 0; i < hahaha.length; i++) {
+//     console.log(hahaha[i]);
+// }
+
+// for (var i = 0; i <= 2; i++) {
+//     console.log(hahaha[i]);
+// }
+
+// for (var i = 3; i < hahaha.length; i++) {
+//     console.log(hahaha[i]);
+// }
+
+for (var i = 1; i <= 4; i++) {
+    console.log(hahaha[i]);
+}
